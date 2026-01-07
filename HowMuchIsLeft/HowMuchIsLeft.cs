1e38a6a80701f51d8cb041424199ca03cdd83892
Arthur Guazzelli <arthur.b.guazzelli@gmail.com>
2026-01-07 19:32:56 -0300

    feat: add support for MWC

--- HowMuchIsLeft/HowMuchIsLeft.cs
+++ HowMuchIsLeft/HowMuchIsLeft.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 
 namespace HowMuchIsLeft
 {
+
+    namespace API
+    {
+        public static class ItemRegistry
+        {
+            private static Dictionary<string, Action<GameObject>> itemHandlers = new Dictionary<string, Action<GameObject>>();
+            public static void RegisterItem(string itemName, Action<GameObject> handler)
+            {
+                itemHandlers[itemName] = handler;
+            }
+
+            public static bool TryGetItemHandler(string itemName, out Action<GameObject> handler)
+            {
+                return itemHandlers.TryGetValue(itemName, out handler);
+            }
+        }
+        
+    }
+
     public class HowMuchIsLeft : Mod
     {
         public override string ID => "HowMuchIsLeft"; // Your (unique) mod ID 
         public override string Name => "HowMuchIsLeft"; // Your mod name
         public override string Author => "casper-3"; // Name of the Author (your name)
-        public override string Version => "1.2.0"; // Version
+        public override string Version => "1.3.0"; // Version
         public override string Description => "Displays the contents of some items. See settings to customize."; // Short description of your mod
+
+        public override Game SupportedGames => Game.MySummerCar | Game.MyWinterCar;
 
         GameObject contentDescription;
         TextMesh foregroundText;
@@ -27,7 +48,7 @@ namespace HowMuchIsLeft
 
         private readonly int LayerItem = 19;
 
-        private static void GenerateText(double amount, double maxAmount, string name, string namePlural = null, Func<Double, Double> f = null, bool forceExact = false)
+        public static void GenerateText(double amount, double maxAmount, string name, string namePlural = null, Func<Double, Double> f = null, bool forceExact = false)
         {
             if (forceExact | detailsExactSetting.GetValue()) {
                 var fAmount = f != null ? f(amount) : amount;
@@ -48,19 +69,19 @@ namespace HowMuchIsLeft
                 text = EducatedGuessText(amountNormalized);
         }
 
-        private static void HandleUncountableItem(GameObject item, string fsmVar, float maxAmount, string name, string namePlural = null, Func<double, double> f = null)
+        public static void HandleUncountableItem(GameObject item, string fsmVar, float maxAmount, string name, string namePlural = null, Func<double, double> f = null)
         {
             float amount = item.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmFloat(fsmVar).Value;
             GenerateText(amount, maxAmount, name, namePlural, f);
         }
 
-        private static void HandleCountableItem(GameObject item, string fsmVar, int maxAmount, string name, string namePlural = null, Func<double, double> f = null)
+        public static void HandleCountableItem(GameObject item, string fsmVar, int maxAmount, string name, string namePlural = null, Func<double, double> f = null)
         {
             int amount = item.GetComponent<PlayMakerFSM>().FsmVariables.FindFsmInt(fsmVar).Value;
             GenerateText(amount, maxAmount, name, namePlural, f, alwaysExactCountablesSetting.GetValue());
         }
 
-        private static void FireExtinguisherHandler(GameObject item, string fsmVar, float maxAmount, string name, string namePlural = null, Func<double, double> f = null)
+        private static void HandleFireExtinguisher(GameObject item, string fsmVar, float maxAmount, string name, string namePlural = null, Func<double, double> f = null)
         {
             PlayMakerFSM[] fsms;
             HutongGames.PlayMaker.FsmFloat amount;
@@ -76,7 +97,7 @@ namespace HowMuchIsLeft
             }
         }
 
-        private static string Pluralize(double amount, string singular, string plural = null)
+        public static string Pluralize(double amount, string singular, string plural = null)
         {
             if (amount == 1)
                 return singular;
@@ -87,12 +108,12 @@ namespace HowMuchIsLeft
             return $"{singular}s";
         }
 
-        private static string ExactValueText(double amount, string name)
+        public static string ExactValueText(double amount, string name)
         {
             return $"{amount:0.##} {name} remaining";
         }
 
-        private static string RoughGuessText(double value)
+        public static string RoughGuessText(double value)
         {
             if (value == 1.0)
                 return "it's full";
@@ -104,7 +125,7 @@ namespace HowMuchIsLeft
                 return "there's still some left";
         }
 
-        private static string EducatedGuessText(double value)
+        public static string EducatedGuessText(double value)
         {
             if (value == 1.0)
                 return "it's full";
@@ -124,25 +145,42 @@ namespace HowMuchIsLeft
                 return "it's almost empty";
         }
 
-        private readonly Dictionary<string, Action<GameObject>> itemHandlerMap = new Dictionary<string, Action<GameObject>>
+        private void RegisterCommonItems()
         {
-            { "coolant(itemx)", (item) => HandleUncountableItem(item, "Fluid", 10f, "liter") },
-            { "motor oil(itemx)", (item) => HandleUncountableItem(item, "Fluid", 4f, "liter") },
-            { "brake fluid(itemx)", (item) => HandleUncountableItem(item, "Fluid", 1f, "liter") },
-            { "two stroke fuel(itemx)", (item) => HandleUncountableItem(item, "Fluid", 5f, "liter")},
-            { "ground coffee(itemx)", (item) => HandleUncountableItem(item, "Ground", 100f, "gram", f: (x) => x * 5f)},
-            { "grill charcoal(itemx)", (item) => HandleUncountableItem(item, "Contents", 140f, "liter", f: (x) => x / 10f)},
-            { "spray can(itemx)", (item) => HandleUncountableItem(item, "Fluid", 100f, "unit") },
-            { "mosquito spray(itemx)", (item) => HandleUncountableItem(item, "Fluid", 100f, "unit") },
+            API.ItemRegistry.RegisterItem("coolant(itemx)", (item) => HandleUncountableItem(item, "Fluid", 10f, "liter"));
+            API.ItemRegistry.RegisterItem("motor oil(itemx)", (item) => HandleUncountableItem(item, "Fluid", 4f, "liter"));
+            API.ItemRegistry.RegisterItem("brake fluid(itemx)", (item) => HandleUncountableItem(item, "Fluid", 1f, "liter"));
+            API.ItemRegistry.RegisterItem("two stroke fuel(itemx)", (item) => HandleUncountableItem(item, "Fluid", 5f, "liter"));
+            API.ItemRegistry.RegisterItem("ground coffee(itemx)", (item) => HandleUncountableItem(item, "Ground", 100f, "gram", f: (x) => x * 5f));
+            API.ItemRegistry.RegisterItem("grill charcoal(itemx)", (item) => HandleUncountableItem(item, "Contents", 140f, "liter", f: (x) => x / 10f));
+            API.ItemRegistry.RegisterItem("spray can(itemx)", (item) => HandleUncountableItem(item, "Fluid", 100f, "unit"));
+            API.ItemRegistry.RegisterItem("mosquito spray(itemx)", (item) => HandleUncountableItem(item, "Fluid", 100f, "unit"));
 
-            { "fuse package(Clone)", (item) => HandleCountableItem(item, "Quantity", 5, "fuse") },
-            { "r20 battery box(Clone)", (item) => HandleCountableItem(item, "Quantity", 4, "battery", namePlural: "batteries" )},
-            { "spark plug box(Clone)", (item) => HandleCountableItem(item, "Quantity", 4, "spark plug") },
-            { "teimo advert pile(itemx)", (item) => HandleCountableItem(item, "Sheets", 30, "sheet") },
-            
-            { "fire extinguisher(itemx)", (item) => FireExtinguisherHandler(item, "Fluid", 100f, "unit") },
+            API.ItemRegistry.RegisterItem("fuse package(Clone)", (item) => HandleCountableItem(item, "Quantity", 5, "fuse"));
+            API.ItemRegistry.RegisterItem("r20 battery box(Clone)", (item) => HandleCountableItem(item, "Quantity", 4, "battery", namePlural: "batteries"));
+            API.ItemRegistry.RegisterItem("spark plug box(Clone)", (item) => HandleCountableItem(item, "Quantity", 4, "spark plug"));
 
-        };
+            API.ItemRegistry.RegisterItem("fire extinguisher(itemx)", (item) => HandleFireExtinguisher(item, "Fluid", 100f, "unit"));
+        }
+
+        private void RegisterMySummerCarItems()
+        {
+            RegisterCommonItems();
+            API.ItemRegistry.RegisterItem("teimo advert pile(itemx)", (item) => HandleCountableItem(item, "Sheets", 30, "sheet"));
+        }
+
+        private void RegisterMyWinterCarItems()
+        {
+            RegisterCommonItems();
+            API.ItemRegistry.RegisterItem("automatic transmission fluid(itemx)", (item) => HandleUncountableItem(item, "Fluid", 1f, "liter"));
+
+            API.ItemRegistry.RegisterItem("advert pile(itemx)", (item) => HandleCountableItem(item, "Sheets", 30, "sheet"));
+            API.ItemRegistry.RegisterItem("chargers box(Clone)", (item) => HandleCountableItem(item, "Items", 40, "charger"));
+            API.ItemRegistry.RegisterItem("packaging sheets(Clone)", (item) => HandleCountableItem(item, "Items", 20, "sheet"));
+            API.ItemRegistry.RegisterItem("manuals box(Clone)", (item) => HandleCountableItem(item, "Items", 80, "manual"));
+            API.ItemRegistry.RegisterItem("plastic trays(Clone)", (item) => HandleCountableItem(item, "Items", 20, "tray"));
+            // TODO: Packages box, look into the different logic 
+        }
 
         public override void ModSetup()
         {
@@ -168,6 +206,11 @@ namespace HowMuchIsLeft
         {
             // Called once, when mod is loading after game is fully loaded
             InitializeDescription();
+
+            if (ModLoader.CurrentGame == Game.MySummerCar)
+                RegisterMySummerCarItems();
+            else if (ModLoader.CurrentGame == Game.MyWinterCar)
+                RegisterMyWinterCarItems();
         }
 
         private void Mod_Update()
@@ -187,7 +230,7 @@ namespace HowMuchIsLeft
 
             GameObject item = raycastHit.transform.gameObject;
 
-            if (itemHandlerMap.TryGetValue(item.name, out Action<GameObject> handler))
+            if (API.ItemRegistry.TryGetItemHandler(item.name, out Action<GameObject> handler))
                 handler(item);
 
             UpdateDescription(text);
